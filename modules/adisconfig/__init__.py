52ca208443c7fc455ab3266440f4a2b1ffcff865
Adrian Chojnicki <adrian@chojnicki.info>
2024-02-18 09:21:57 +0000

    changes

--- modules/adisconfig/__init__.py
+++ modules/adisconfig/__init__.py
@@ -1,5 +1,6 @@
-from yaml import safe_load
+from yaml import safe_load, dump
 from pathlib import Path
+from pprint import pformat
 
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
@@ -23,6 +24,17 @@ class adisconfig:
             
             for item in config_data:
                 self._config[item]=AttrDict(config_data[item])
-                
+    
+    def save(self):
+        d={}
+        for data in self._config:
+            d[data]=dict(self._config[data])
+
+        with open(self._config_file,'w') as config_file:
+            dump(d, config_file, sort_keys=False)
+
     def __getattr__(self, attr):
         return self._config[attr]
+
+    def __repr__(self):
+        return pformat(self._config)
